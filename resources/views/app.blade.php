1611ee3ea8edc73550327e9e51a7cce0c85d89d8
arnaullfe <arnaullfe@gmail.com>
2021-03-03 22:34:36 +0100

    afegint vue 9:48

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Sanctum App
                 </div>
 
                 <div class="links">
